f13297d37244d8f9f2a61811c617c24fa40543e2
laxmikishore13 <laxmikishore1@gmail.com>
2023-09-26 23:41:58 +0530

    array methods

--- index1.js
+++ index1.js
@@ -10,12 +10,14 @@ for (let link of links) {
   link.addEventListener("click", () => linkClickHandler(link.innerText));
 }
 
+let arr = new Array(8).fill(10);
+
 /**
  * hoisting - var, let, const
  * event loops
  * call stack
  * closures
- *
+ * proptypes
  * functions -> arrow functions, anonymous function
  *
  * var, let, const
